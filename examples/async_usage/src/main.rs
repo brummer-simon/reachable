e348c31a84b2f3b12609f716f70e6f3055e0af7b
Simon Brummer <simon.brummer@posteo.de>
2021-10-17 14:53:28 +0200

    Use MPL instead of GPL

--- examples/async_usage/src/main.rs
+++ examples/async_usage/src/main.rs
@@ -1,4 +1,10 @@
 use std::str::FromStr;
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// Author: Simon Brummer (simon.brummer@posteo.de)
+
 use std::thread::sleep;
 use std::time::Duration;
 
